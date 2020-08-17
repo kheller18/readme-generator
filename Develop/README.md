1b1824706b7cf70b9927a5984ef7f373780ffafb
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-08-16 20:29:47 -0700

    added questions

---
+++ Develop/README.md
@@ -0,0 +1,38 @@
+# dkdk
+
+ddkdk
+
+## Table of Contents
+
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [License](#license)
+  4. [Contributing](#contributing)
+  5. [Tests](#tests)
+  6. [Questions](#questions)
+
+### Installation
+
+dkdkd
+
+### Usage
+
+dkdkd
+
+### License
+
+k
+
+### Contributing
+
+dkk
+
+### Tests
+
+dkk
+
+### Questions
+
+[GitHub: kheller18](https://github.com/kheller18)
+
+Email: keenanaheller@gmail.com
---
+++ Develop/index.js
@@ -0,0 +1,133 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user
+function promptUser() {
+    return inquirer.prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "Project Name:"
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Project Description:"
+        },
+        {
+          type: "input",
+          name: "installationInstructions",
+          message: "Installation instructions:"
+        },
+        {
+          type: "input",
+          name: "usageInformation",
+          message: "Usage information:"
+        },
+        {
+          type: "input",
+          name: "contributionGuidelines",
+          message: "Contribution guidelines:"
+        },
+        {
+          type: "input",
+          name: "testInstructions",
+          message: "Test instructions:"
+        },
+        {
+        type: "input",
+        name: "license",
+        message: "License:"
+        },
+        {
+        type: "input",
+        name: "githubUsername",
+        message: "Github username:"
+        },
+        {
+        type: "input",
+        name: "emailAddress",
+        message: "Email address:"
+        }  
+    ]);
+}
+
+// function to write README file
+// function writeToFile(fileName, data) {
+//     return `
+//   ${ data.title }
+    
+//   `;
+// }
+
+// function to initialize program
+// function init() {
+
+// }
+
+//function to write README file
+function writeToFile(data) {
+  return `# ${ data.title }\n
+${ data.description }\n
+## Table of Contents\n
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)\n
+### Installation\n
+${data.installationInstructions}\n
+### Usage\n
+${ data.usageInformation }\n
+### License\n
+${ data.license }\n
+### Contributing\n
+${ data.contributionGuidelines }\n
+### Tests\n
+${ data.testInstructions }\n
+### Questions\n
+[GitHub: ${ data.githubUsername }](https://github.com/${ data.githubUsername })\n
+Email: ${ data.emailAddress }\n`
+;
+}
+
+// function writeToFile(fileName, data) {
+//   return `
+// ${ data.title }
+  
+// `;
+// }
+
+
+promptUser()
+  .then(function(data) {
+    // console.log(data);
+    const sample = writeToFile(data);
+    console.log(sample);
+    return writeFileAsync("README.md", sample);
+
+  })
+  .then(function() {
+    // console.log("successful");
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+// function call to initialize program
+// init()
+    // .then(function(answers) {
+    //     const html = generateHTML(answers);
+
+    //     return writeFileAsync("index.html", html);
+    // })
+    // .then(function() {
+    //     console.log("Successfully wrote to index.html");
+    // })
+    // .catch(function(err) {
+    //     console.log(err);
+    // });
+// ;    
