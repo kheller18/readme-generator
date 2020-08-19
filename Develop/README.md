c815508d1fe24ce2791844f96786e480dae5c0ce
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-08-18 23:04:02 -0700

    added more licenses and updated readme output

--- Develop/README.md
+++ Develop/README.md
@@ -1,43 +1,67 @@
+
 # Readme Generator
-  ![alt text](https://img.shields.io/github/license/kheller18/Readme Generator?style=plastic)
+![alt text](https://img.shields.io/github/license/kheller18/readme-generator?style=plastic)
 
 
 ### Description
-  A generator for your readmes
+  This application makes quick and simple readmes
 
 
 ## Table of Contents
-  1. [Installation](#1 installation)
-  2. [Usage](#2 usage)
-  3. [License](#3 license)
-  4. [Contributing](#4 contributing)
-  5. [Tests](#5 tests)
-  6. [Questions](#6 questions)
+  1. [Installation](#1-installation)
+  2. [Usage](# 2-usage)
+  3. [License](#3-license)
+  4. [Contributing](#4-contributing)
+  5. [Tests](#5-tests)
+  6. [Questions](#6-questions)
 
 
 ## 1. Installation
-  install
+  Download the repository
 
 
 ## 2. Usage
-  use  
+  npm install, then type "node index.js" and press enter.  
   
 
-
 ## 3. License
+	
+      MIT License
 
+      Copyright (c) 2020 Keenan Heller
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.
+      
 
 
 ## 4. Contributing
-  feel free
+  * [Keenan Heller](https://github.com/kheller18)
 
 
 ## 5. Tests
-  no tests currently
+  No current tests
 
 
 ## 6. Questions
-  [GitHub: kheller18](https://github.com/kheller18)
-  Email: keenanaheller@gmail.com
+
+  * [GitHub: kheller18](https://github.com/kheller18)
+
+  * Email: keenanaheller@gmail.com
     * Please feel free to contact me at the email address provided above.  I will be happy to answer any questions.  Feedback is gladly welcomed and appreciated!
   
--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const license = require("./license")
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -57,7 +56,7 @@ function promptUser() {
         type: "list",
         name: "license",
         message: "What license is your application under?",
-        choices: ['MIT', 'Apache', 'GNU GPLv3']
+        choices: ['GNU AGPv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
       },
       {
         type: "input",
@@ -77,11 +76,191 @@ function promptUser() {
     ]);
 }
 
+function license(type, userName) {
+  switch(type) {
+    case "GNU AGPv3":
+      return `
+      Copyright (C) <year>  ${ userName }
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU Affero General Public License as published
+      by the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU Affero General Public License for more details.
+
+      You should have received a copy of the GNU Affero General Public License
+      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+      `
+
+    case "GNU GPLv3":
+      return `
+      Copyright (C) <year>  ${ userName }
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+  
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+  
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+      `
+
+    case "GNU LGPLv3":
+      return `
+      Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+      Everyone is permitted to copy and distribute verbatim copies
+      of this license document, but changing it is not allowed.
+      `
+
+    case "Mozilla Public License 2.0":
+      return `
+      This Source Code Form is subject to the terms of the Mozilla Public
+      License, v. 2.0. If a copy of the MPL was not distributed with this file,
+      You can obtain one at https://mozilla.org/MPL/2.0/.
+      `
+
+    case "Apache License 2.0":
+      return `
+      Copyright [yyyy] ${ userName }
+
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
+      `
+
+    case "MIT License":
+      return `
+      MIT License
+
+      Copyright (c) 2020 ${ userName }
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.
+      `
+  
+    case "Boost Software License 1.0":
+      return `
+      Boost Software License - Version 1.0 - August 17th, 2003
+
+      Permission is hereby granted, free of charge, to any person or organization
+      obtaining a copy of the software and accompanying documentation covered by
+      this license (the "Software") to use, reproduce, display, distribute,
+      execute, and transmit the Software, and to prepare derivative works of the
+      Software, and to permit third-parties to whom the Software is furnished to
+      do so, all subject to the following:
+
+      The copyright notices in the Software and this entire statement, including
+      the above license grant, this restriction and the following disclaimer,
+      must be included in all copies of the Software, in whole or in part, and
+      all derivative works of the Software, unless such copies or derivative
+      works are solely in the form of machine-executable object code generated by
+      a source language processor.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+      SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+      FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+      ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+      DEALINGS IN THE SOFTWARE.
+      `
+
+    case "The Unlicense":
+      return `
+      This is free and unencumbered software released into the public domain.
+
+      Anyone is free to copy, modify, publish, use, compile, sell, or
+      distribute this software, either in source code form or as a compiled
+      binary, for any purpose, commercial or non-commercial, and by any
+      means.
+
+      In jurisdictions that recognize copyright laws, the author or authors
+      of this software dedicate any and all copyright interest in the
+      software to the public domain. We make this dedication for the benefit
+      of the public at large and to the detriment of our heirs and
+      successors. We intend this dedication to be an overt act of
+      relinquishment in perpetuity of all present and future rights to this
+      software under copyright law.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+      IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+      OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+      ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+      OTHER DEALINGS IN THE SOFTWARE.
+
+      For more information, please refer to <https://unlicense.org>
+      `
+
+    default:
+      return `
+      This is free and unencumbered software released into the public domain.
+
+      Anyone is free to copy, modify, publish, use, compile, sell, or
+      distribute this software, either in source code form or as a compiled
+      binary, for any purpose, commercial or non-commercial, and by any
+      means.
+
+      In jurisdictions that recognize copyright laws, the author or authors
+      of this software dedicate any and all copyright interest in the
+      software to the public domain. We make this dedication for the benefit
+      of the public at large and to the detriment of our heirs and
+      successors. We intend this dedication to be an overt act of
+      relinquishment in perpetuity of all present and future rights to this
+      software under copyright law.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+      IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+      OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+      ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+      OTHER DEALINGS IN THE SOFTWARE.
+
+      For more information, please refer to <https://unlicense.org>
+      `
+  }
+}
+
 
 //function to write README file
-function writeToFile(data) {
-  return `# ${ data.title }
-  ![alt text](https://img.shields.io/github/license/${ data.githubUsername }/${ data.repositoryName }?style=plastic)
+function writeToFile(data, licenseInfo) {
+  return `
+# ${ data.title }
+![alt text](https://img.shields.io/github/license/${ data.githubUsername }/${ data.repositoryName }?style=plastic)
 
 
 ### Description
@@ -89,12 +268,12 @@ function writeToFile(data) {
 
 
 ## Table of Contents
-  1. [Installation](#1 installation)
-  2. [Usage](#2 usage)
-  3. [License](#3 license)
-  4. [Contributing](#4 contributing)
-  5. [Tests](#5 tests)
-  6. [Questions](#6 questions)
+  1. [Installation](#1-installation)
+  2. [Usage](#2-usage)
+  3. [License](#3-license)
+  4. [Contributing](#4-contributing)
+  5. [Tests](#5-tests)
+  6. [Questions](#6-questions)
 
 
 ## 1. Installation
@@ -106,7 +285,7 @@ function writeToFile(data) {
   
 
 ## 3. License
-
+\t${ licenseInfo }
 
 
 ## 4. Contributing
@@ -117,9 +296,9 @@ function writeToFile(data) {
   ${ data.testInstructions }
 
 
-## 6. Questions
-  [GitHub: ${ data.githubUsername }](https://github.com/${ data.githubUsername })
-  Email: ${ data.emailAddress }
+## 6. Questions\n
+  * [GitHub: ${ data.githubUsername }](https://github.com/${ data.githubUsername })\n
+  * Email: ${ data.emailAddress }
     * Please feel free to contact me at the email address provided above.  I will be happy to answer any questions.  Feedback is gladly welcomed and appreciated!
   `
 ;
@@ -129,7 +308,9 @@ function init() {
   promptUser()
   .then(function(data) {
     // console.log(data);
-    const sample = writeToFile(data);
+    const licenseText = license(data.license, data.userName);
+    const sample = writeToFile(data, licenseText);
+    // console.log(licenseText);
     // console.log(sample);
     return writeFileAsync("README.md", sample);
   })
