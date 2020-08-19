da75d8aa5e007344b334d374bc67cc4bc4f94fb2
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-08-18 21:43:50 -0700

    updated questions and readme format

--- Develop/README.md
+++ Develop/README.md
@@ -1,42 +1,43 @@
-# fk
+# Readme Generator
+  ![alt text](https://img.shields.io/github/license/kheller18/Readme Generator?style=plastic)
 
-d
+
+### Description
+  A generator for your readmes
+
 
 ## Table of Contents
-
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-
-### Installation
+  1. [Installation](#1 installation)
+  2. [Usage](#2 usage)
+  3. [License](#3 license)
+  4. [Contributing](#4 contributing)
+  5. [Tests](#5 tests)
+  6. [Questions](#6 questions)
 
 
-
-### Usage
-
-
-
-### License
-
-Licensed under MIT ![alt text](https://img.shields.io/github/license//fk?style=plastic)
+## 1. Installation
+  install
 
 
-### Contributing
+## 2. Usage
+  use  
+  
+
+
+## 3. License
 
 
 
-### Tests
+## 4. Contributing
+  feel free
 
 
+## 5. Tests
+  no tests currently
 
-### Questions
 
-[GitHub: ](https://github.com/)
-
-Email: 
-
-  * Please feel free to contact me at the email address provided above.  I will be happy to answer any questions.  Feedback is gladly welcomed.
+## 6. Questions
+  [GitHub: kheller18](https://github.com/kheller18)
+  Email: keenanaheller@gmail.com
+    * Please feel free to contact me at the email address provided above.  I will be happy to answer any questions.  Feedback is gladly welcomed and appreciated!
   
--- Develop/index.js
+++ Develop/index.js
@@ -1,129 +1,143 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const license = require("./license")
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 function promptUser() {
     return inquirer.prompt([
-        {
-          type: "input",
-          name: "title",
-          message: "Project name:"
-        },
-        {
-          type: "input",
-          name: "description",
-          message: "Project description:"
-        },
-        {
-          type: "input",
-          name: "screenshot",
-          message: "Screenshot of your application (url):"
-        },
-        {
-          type: "input",
-          name: "installationInstructions",
-          message: "Installation instructions:"
-        },
-        {
-          type: "input",
-          name: "usageInformation",
-          message: "Usage information:"
-        },
-        {
-          type: "input",
-          name: "contributionGuidelines",
-          message: "Contribution guidelines:"
-        },
-        {
-          type: "input",
-          name: "testInstructions",
-          message: "Test instructions:"
-        },
-        {
-          type: "list",
-          name: "license",
-          message: "License:",
-          choices: ['MIT', 'Apache', 'GNU GPLv3']
-        },
-        {
-          type: "input",
-          name: "githubUsername",
-          message: "Github username:"
-        },
-        {
-          type: "input",
-          name: "emailAddress",
-          message: "Email address:"
-        }  
+      {
+        type: "input",
+        name: "repositoryUrl",
+        message: "What is the repository URL associated with this project?"
+      },
+      {
+        type: "input",
+        name: "repositoryName",
+        message: "What is the name of your repository?"
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "How would you describe this project?"
+      },
+      {
+        type: "input",
+        name: "screenshot",
+        message: "What is the URL of a screenshot of your application?"
+      },
+      {
+        type: "input",
+        name: "installationInstructions",
+        message: "What are the installation instructions?"
+      },
+      {
+        type: "input",
+        name: "usageInformation",
+        message: "Usage information:"
+      },
+      {
+        type: "input",
+        name: "contributionGuidelines",
+        message: "Who contributed to the development of this application?"
+      },
+      {
+        type: "input",
+        name: "testInstructions",
+        message: "What are the test instructions?"
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What license is your application under?",
+        choices: ['MIT', 'Apache', 'GNU GPLv3']
+      },
+      {
+        type: "input",
+        name: "userName",
+        message: "What is your name?"
+      },
+      {
+        type: "input",
+        name: "githubUsername",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "input",
+        name: "emailAddress",
+        message: "What is your email address?"
+      }  
     ]);
 }
 
 
 //function to write README file
 function writeToFile(data) {
-  return `# ${ data.title }\n
-${ data.description }\n
-## Table of Contents\n
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)\n
-### Installation\n
-${data.installationInstructions}\n
-### Usage\n
-${ data.usageInformation }\n
-### License\n
-Licensed under ${ data.license } ![alt text](https://img.shields.io/github/license/${ data.githubUsername }/${ data.title }?style=plastic)
-\n
-### Contributing\n
-${ data.contributionGuidelines }\n
-### Tests\n
-${ data.testInstructions }\n
-### Questions\n
-[GitHub: ${ data.githubUsername }](https://github.com/${ data.githubUsername })\n
-Email: ${ data.emailAddress }\n
-  * Please feel free to contact me at the email address provided above.  I will be happy to answer any questions.  Feedback is gladly welcomed.
-  `
+  return `# ${ data.title }
+  ![alt text](https://img.shields.io/github/license/${ data.githubUsername }/${ data.repositoryName }?style=plastic)
+
+
+### Description
+  ${ data.description }
+
+
+## Table of Contents
+  1. [Installation](#1 installation)
+  2. [Usage](#2 usage)
+  3. [License](#3 license)
+  4. [Contributing](#4 contributing)
+  5. [Tests](#5 tests)
+  6. [Questions](#6 questions)
+
+
+## 1. Installation
+  ${data.installationInstructions}
+
+
+## 2. Usage
+  ${ data.usageInformation }  
   
+
+## 3. License
+
+
+
+## 4. Contributing
+  * [${ data.userName }](https://github.com/${ data.githubUsername })
+
+
+## 5. Tests
+  ${ data.testInstructions }
+
+
+## 6. Questions
+  [GitHub: ${ data.githubUsername }](https://github.com/${ data.githubUsername })
+  Email: ${ data.emailAddress }
+    * Please feel free to contact me at the email address provided above.  I will be happy to answer any questions.  Feedback is gladly welcomed and appreciated!
+  `
 ;
 }
-
-// function getBadge(data) {
-//   let url = '';
-//   switch(data.license) {
-//     case 'MIT': 
-//       url = `(https://img.shields.io/github/license/${ data.githubUsername }/${ data.title }?style=plastic)`
-//       break;
-//     case "Apache":
-//       url = `https://shields.io/category/license/aur/license/:${ npmPackage }`
-//       break;
-//     case "GNU GPLv3":
-//       url = `https://shields.io/category/license/cran/l/:${ npmPackage }`
-//       break;
-//   }
-//   return url;
-// }
 
 function init() {
   promptUser()
   .then(function(data) {
-    console.log(data);
-    // licenseType = getBadge(data);
+    // console.log(data);
     const sample = writeToFile(data);
-    console.log(sample);
-    // console.log(licenseType);
+    // console.log(sample);
     return writeFileAsync("README.md", sample);
   })
   .then(function() {
-    // console.log("successful");
+    console.log("README creation succeeded!");
   })
   .catch(function(error) {
-    console.log(error);
+    console.log("README creation unsuccessful.");
   });
 }
 
